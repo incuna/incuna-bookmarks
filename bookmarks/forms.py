3fe4cb6fbafe69b9e7520466b7e7e2d405cf0ed0
mattack108 <matt.lenc@gmail.com>
2013-02-07 10:25:33 +0000

    Make URLField compatible with Django 1.4 and remove verify_exists attribute

--- bookmarks/forms.py
+++ bookmarks/forms.py
@@ -7,7 +7,7 @@ from bookmarks.models import Bookmark, BookmarkInstance
 
 class BookmarkInstanceForm(forms.ModelForm):
     
-    url = forms.URLField(label = "URL", verify_exists=True, widget=forms.TextInput(attrs={"size": 40}))
+    url = forms.URLField(label = "URL", widget=forms.TextInput(attrs={"size": 40}))
     description = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"size": 40}))
     redirect = forms.BooleanField(label="Redirect", required=False)
     tags = TagField(label="Tags", required=False)
