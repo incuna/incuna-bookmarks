ca226045e31d001053c0ee8dd32ef9f4b967e8c3
Ian Foote <ian@incuna.com>
2016-02-10 10:40:55 +0000

    Refactor bookmarks.urls for style

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,21 +1,26 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
 
 # for voting
 from voting.views import vote_on_object
 from bookmarks.models import Bookmark
+from bookmarks import views
 
-urlpatterns = patterns('',
-    url(r'^$', 'bookmarks.views.bookmarks', name="all_bookmarks"),
-    url(r'^your_bookmarks/$', 'bookmarks.views.your_bookmarks', name="your_bookmarks"),
-    url(r'^add/$', 'bookmarks.views.add', name="add_bookmark"),
-    url(r'^(\d+)/delete/$', 'bookmarks.views.delete', name="delete_bookmark_instance"),
+urlpatterns = [
+    url(r'^$', views.bookmarks, name="all_bookmarks"),
+    url(r'^your_bookmarks/$', views.your_bookmarks, name="your_bookmarks"),
+    url(r'^add/$', views.add, name="add_bookmark"),
+    url(r'^(\d+)/delete/$', views.delete, name="delete_bookmark_instance"),
 
     # for voting
-    (r'^(?P<object_id>\d+)/(?P<direction>up|down|clear)vote/?$',
-        csrf_exempt(vote_on_object), dict(
-            model=Bookmark,
-            template_object_name='bookmark',
-            template_name='kb/link_confirm_vote.html',
-            allow_xmlhttprequest=True)),
-)
+    url(
+        r'^(?P<object_id>\d+)/(?P<direction>up|down|clear)vote/?$',
+        csrf_exempt(vote_on_object),
+        {
+            'model': Bookmark,
+            'template_object_name': 'bookmark',
+            'template_name': 'kb/link_confirm_vote.html',
+            'allow_xmlhttprequest': True,
+        },
+    ),
+]
